abc1311201f903c0e1b73ca7ec42954c93e034f4
Brooks <brooksnew92@gmail.com>
2024-06-05 08:28:21 +0000

    added environment.yaml

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+conda create --name MFRNP python=3.10.11
+
 conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 
-pip install numpy glob2 pyyaml wandb pyyaml ray[tune] matplotlib
+pip install numpy glob2 pyyaml wandb ray[tune] matplotlib
