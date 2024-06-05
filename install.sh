1e135f7ae0a2df35b5b74a9e854c3d4de7b36301
Brooks <brooksnew92@gmail.com>
2024-06-05 08:32:18 +0000

    removed install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-conda create --name MFRNP python=3.10.11
-
-conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-
-pip install numpy glob2 pyyaml wandb ray[tune] matplotlib
